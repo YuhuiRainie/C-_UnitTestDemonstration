7ec24e5142dcf6fae7e2f756664f0c29fd70f086
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2020-08-07 11:25:04 +1000

    unit_test_demonstration

---
+++ Shapes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace UnitTestDemonstration
+{
+    public class Shapes
+    {
+        // Private fields
+        private double height, width, radius;
+        private const double pi = 3.142; // Define the value of pi
+
+        //Constructor to create a Circle
+        public Shapes(double rad)
+        {
+            radius = rad;
+        }
+        //Constructor to create a rectangle
+        public Shapes(double ht, double wd)
+        {
+            height = ht;
+            width = wd;
+        }
+        // Method to calculate the area of Rectangle
+        public double RectangleArea()
+        {
+            return (height * width);
+        }
+        // Method to calculate the area of Circle
+        public double CircleArea()
+        {
+            return (pi * radius*radius);
+        }
+        public void Display(char shapeType)
+        {
+            if (shapeType == 'r')
+                Console.WriteLine("Height and Width of rectangle is: {0}, {1}", height, width);
+            else if(shapeType == 'c')
+                Console.WriteLine("Radius of circle is: {0}", radius);
+        }
+    }
+    class ShapesTest
+    {
+        static void Main(string[] args)
+        {
+            // Create Shape object as per test case
+            Shapes circle = new Shapes(4.0);
+            Shapes rectangle = new Shapes(5.0, 4.0);
+
+            // Display the object properties
+            circle.Display('c');
+            rectangle.Display('r');
+
+            // Calculate and display the area of the objects
+            Console.WriteLine("The area of circle is {0}", circle.CircleArea());
+            Console.WriteLine("The area of rectangle is {0}", rectangle.RectangleArea());
+
+            // Accept a key press from user
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemonstration;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var sh = new Shapes(4.0);
+            Assert.AreEqual(sh.CircleArea(),50.272);
+
+  
+        }
+    }
+}
